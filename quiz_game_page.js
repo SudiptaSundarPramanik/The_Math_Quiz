75e3b9dc95120c5d0738d238c65347d3b48657f3
SudiptaSundarPramanik <79316024+SudiptaSundarPramanik@users.noreply.github.com>
2021-06-21 19:56:59 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -10,18 +10,23 @@ document.getElementById("player_question").innerHTML="Question Turn-"+player1_na
 document.getElementById("player_answer").innerHTML="Answer Turn-"+player2_name;
 function send(){
     number1=document.getElementById("number1").value;
+    console.log(number1);
     number2=document.getElementById("number2").value;
-    actual_answer=parseInt(number1) * parseInt(number2);
+    console.log(number2);
+    actual_answer=parseInt(number1)*parseInt(number2);
+    console.log(actual_answer);
     question_number="<h4>"+number1+" X "+number2+"</h4>";
+    console.log(question_number);
     input_box="<br>Answer : <input type='text' id='input_check_box'>";
+    console.log(input_box);
     check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row=question_number+input_box+check_button;
     document.getElementById("output").innerHTML=row;
     document.getElementById("number1").value="";
     document.getElementById("number2").value="";
 }
-question_turn="player1_name";
-answer_turn="player2_name";
+question_turn="player1";
+answer_turn="player2";
 function check()
 {
     get_answer=document.getElementById("input_check_box").value;
@@ -29,13 +34,13 @@ function check()
     {
         if(answer_turn=="player1")
         {
-            update_player1_score=player1_score+1;
-            document.getElementById("player1_score").innerHTML=update_player1_score;
+            player1_score=player1_score+1;
+            document.getElementById("player1_score").innerHTML=player1_score;
         }
         else
         {
-            update_player2_score=player2_score+1;
-            document.getElementById("player2_score").innerHTML=update_player2_score;
+            player2_score=player2_score+1;
+            document.getElementById("player2_score").innerHTML=player2_score;
         }
     }
     if(question_turn=="player1")
@@ -59,4 +64,5 @@ function check()
         answer_turn="player1"
         document.getElementById("player_answer").innerHTML="Answer Turn-"+player1_name;
     }
+    document.getElementById("output").innerHTML="";
 }
